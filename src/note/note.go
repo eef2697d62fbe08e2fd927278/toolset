d40aa593e02a13a6c99e6a5906f648481c1300b1
youngtrashbag <yuule.smeker@gmail.com>
2020-06-24 14:41:53 +0200

    chore: make most attributes public

--- src/note/note.go
+++ src/note/note.go
@@ -1,47 +1,30 @@
 package note
 
-/*
 import (
 	"time"
 )
-*/
 
 // Note : Struct used for writing note
 type Note struct {
-	title   string
-	content string
-	time    time
-	tags    []string
+	Title   string
+	Content string
+	time    time.Time
+	Tags    []string
 }
 
 // NewNote : a constructor for the Note struct
-func NewNote(t string, c string, t time, tg []string) Note {
+func NewNote(t string, c string, tg []string) Note {
 	var n Note
 
-	n.SetTitle(t)
-	n.SetContent(c)
-	n.SetTime(t)
-	n.SetTags(tg)
+	n.Title = t
+	n.Content = c
+	n.SetTime(time.Now())
+	n.Tags = tg
 
 	return n
 }
 
-// SetTitle : set title of note
-func (n *Note) SetTitle(t string) {
-	n.title = t
-}
-
-// SetContent : set content of note
-func (n *Note) SetContent(c string) {
-	n.content = c
-}
-
 // SetTime : sets time of note
-func (n *Note) SetTime(t time) {
+func (n *Note) SetTime(t time.Time) {
 	n.time = t
-}
-
-// SetTags : set tags of note
-func (n *Note) SetTags(t []string) {
-	n.tags = t
 }
