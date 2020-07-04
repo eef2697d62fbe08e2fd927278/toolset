708f2717d8d3f525c95a10f80e28b10dfb4e512e
youngtrashbag <yuule.smeker@gmail.com>
2020-07-04 10:22:39 +0200

    chore: database related functions now in db file

---
+++ src/note/db.go
@@ -0,0 +1,94 @@
+package note
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/youngtrashbag/toolset/src/database"
+	"github.com/youngtrashbag/toolset/src/tag"
+)
+
+// Insert : saves a user in the database and returns the id of said db entry
+func (n *Note) Insert() int64 {
+
+	// connection to database
+	db, err := sql.Open("mysql", "toolset_insert:password@/toolset")
+	if err != nil {
+		log.Panicln(err.Error())
+	}
+	defer db.Close()
+
+	// prepare sql insert note statement
+	insertNote, err := db.Prepare("INSERT INTO tbl_note (title, content, creationDate) VALUES (?, ?, ?)")
+	if err != nil {
+		log.Panicln(err.Error())
+	}
+	defer insertNote.Close()
+
+	var time string
+	database.ConvertTime(&n.CreationDate, &time)
+	// execute sql insert note statement
+	result, err := insertNote.Exec(n.Title, n.Content, time)
+	if err != nil {
+		log.Panicln(err.Error())
+	}
+
+	noteID, err := result.LastInsertId()
+	if err != nil {
+		log.Panicln(err.Error())
+	}
+
+	// TODO: actually take the tags from somewhere real
+	// the tags as strings
+	var tagsS []string
+
+	for _, t := range tagsS {
+		tg := tag.GetByName(t)
+
+		tID := tg.ID
+		// if the note is not yet in the db it will be inserted
+		if tID == -1 {
+			tID = tg.Insert()
+		}
+		tag.LinkNote(noteID, tID)
+	}
+
+	return noteID
+}
+
+// GetByID : returns the selected note from the database as an object
+func GetByID(id int64) Note {
+	db, err := sql.Open("mysql", "toolset_select:password@/toolset")
+	if err != nil {
+		log.Panicln(err.Error())
+	}
+	defer db.Close()
+
+	tagRows, err := db.Query("SELECT id, title, content, creationDate, author FROM tbl_note WHERE id = ?", id)
+	if err != nil {
+		log.Panicln(err.Error())
+	}
+	defer tagRows.Close()
+
+	var n Note
+	var timeStr string
+	for tagRows.Next() {
+		err := tagRows.Scan(&n.ID, &n.Title, &n.Content, &timeStr, &n.AuthorID)
+		if err != nil {
+			log.Panicln(err.Error())
+		}
+	}
+
+	err = tagRows.Err()
+	if err != nil {
+		log.Panicln(err.Error())
+	}
+
+	if n.ID == 0 && n.Content == "" {
+		// when there is no entry found, return id = -1
+		n.ID = -1
+	}
+
+	database.ConvertTime(&n.CreationDate, &timeStr)
+	return n
+}
---
+++ src/tag/db.go
@@ -0,0 +1,117 @@
+package tag
+
+import (
+	"database/sql"
+	"log"
+	"strings"
+)
+
+// Insert : saves tag to db and returns id of entry
+func (t *Tag) Insert() int64 {
+	db, err := sql.Open("mysql", "toolset_insert:password@/toolset")
+	if err != nil {
+		log.Panicln(err.Error())
+	}
+	defer db.Close()
+
+	// prepare sql insert note statement
+	insertNote, err := db.Prepare("INSERT INTO tbl_tag (name, creationDate) VALUES (?, ?)")
+	if err != nil {
+		log.Panicln(err.Error())
+	}
+	defer insertNote.Close()
+
+	// execute sql insert note statement
+	result, err := insertNote.Exec(t.Name, t.CreationDate)
+	if err != nil {
+		log.Panicln(err.Error())
+	}
+
+	tagID, err := result.LastInsertId()
+	if err != nil {
+		log.Panicln(err.Error())
+	}
+
+	return tagID
+}
+
+// GetByID : returns a tag object from the database
+func GetByID(id int64) Tag {
+	db, err := sql.Open("mysql", "toolset_select:password@/toolset")
+	if err != nil {
+		log.Panicln(err.Error())
+	}
+	defer db.Close()
+
+	tagRows, err := db.Query("SELECT id, name FROM tbl.Tag WHERE id = ?", id)
+	if err != nil {
+		log.Panicln(err.Error())
+	}
+	defer tagRows.Close()
+
+	var t Tag
+	for tagRows.Next() {
+		err := tagRows.Scan(&t.ID, &t.Name)
+		if err != nil {
+			log.Panicln(err.Error())
+		}
+	}
+
+	err = tagRows.Err()
+	if err != nil {
+		log.Panicln(err.Error())
+	}
+
+	return t
+}
+
+// GetByName : return a tag searched for by its name
+func GetByName(name string) Tag {
+	db, err := sql.Open("mysql", "toolset_select:password@/toolset")
+	if err != nil {
+		log.Panicln(err.Error())
+	}
+	defer db.Close()
+
+	name = strings.ToLower(name)
+	tagRows, err := db.Query("SELECT id, name FROM tbl.Tag WHERE name = ?", name)
+	if err != nil {
+		log.Panicln(err.Error())
+	}
+	defer tagRows.Close()
+
+	var t Tag
+	for tagRows.Next() {
+		err := tagRows.Scan(&t.ID, &t.Name)
+		if err != nil {
+			log.Panicln(err.Error())
+		}
+	}
+
+	err = tagRows.Err()
+	if err != nil {
+		log.Panicln(err.Error())
+	}
+
+	return t
+}
+
+// LinkNote : this links the noteID and the tagId together via the linktable
+func LinkNote(nID, tID int64) {
+	db, err := sql.Open("mysql", "toolset_insert:password@/toolset")
+	if err != nil {
+		log.Panicln(err.Error())
+	}
+	defer db.Close()
+
+	linkTag, err := db.Prepare("INSERT INTO lktbl_tag (note_id, tag_id) VALUES (?, ?)")
+	if err != nil {
+		log.Panicln(err.Error())
+	}
+	defer linkTag.Close()
+
+	_, err = linkTag.Exec(nID, tID)
+	if err != nil {
+		log.Panicln(err.Error())
+	}
+}
