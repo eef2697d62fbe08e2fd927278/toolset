8c1795b3f97cf3df69fcc233fde0396a0cc23441
Elias Albisser <elias.albisser@siemens.com>
2020-07-06 14:44:50 +0200

    chore: move and rename main handler function

--- src/main.go
+++ src/main.go
@@ -11,8 +11,12 @@ import (
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/", HomeHandler)
+	router.HandleFunc("/", Handler)
 
+	// user frontend handlers
+	//router.HandleFunc("/user", user.Handler)
+
+	// frontend note handlers
 	router.HandleFunc("/note", note.Handler)
 
 	apiRouter := router.PathPrefix("/api").Subrouter()
@@ -30,31 +34,4 @@ func main() {
 	log.Printf("Starting Server on Port \"%s\"", port)
 	defer log.Println("Server shut-down!")
 	log.Fatal(http.ListenAndServe(port, router))
-}
-
-// HomeHandler : Handles API call to root (/)
-func HomeHandler(res http.ResponseWriter, req *http.Request) {
-	var statusCode int
-	var message string
-
-	if req.Method == http.MethodGet {
-		statusCode = 200
-		message = "hello get world"
-	} else if req.Method == http.MethodPost {
-		statusCode = 201
-		message = "hello post world"
-	} else if req.Method == http.MethodPut {
-		statusCode = 202
-		message = "hello put world"
-	} else if req.Method == http.MethodDelete {
-		statusCode = 204
-		message = "hello delete world"
-	} else {
-		statusCode = 404
-	}
-
-	log.Printf("%s Method on \"%s\", StatusCode:%d, Message:\"%s\"", req.Method, req.URL.Path, statusCode, message)
-
-	res.WriteHeader(statusCode)
-	res.Write([]byte(message))
 }
